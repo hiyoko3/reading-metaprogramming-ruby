79cf0862129a0e327e4fbba1d49dbaddc3fbe8fe
hiyoko3 <sb.gm.0823@gmail.com>
2020-03-20 12:59:13 +0900

    Passed 02_object_model

--- 02_object_model/hoge.rb
+++ 02_object_model/hoge.rb
@@ -1,7 +1,14 @@
-class Hoge
+class Hoge < String
   # Hogeクラスの仕様
   # "hoge" という文字列の定数Hogeを持つ
+  Hoge = "hoge".freeze
   # "hoge" という文字列を返すhogehogeメソッドを持つ
+  def hogehoge
+    Hoge
+  end
   # HogeクラスのスーパークラスはStringである
   # 自身が"hoge"という文字列である時（HogeクラスはStringがスーパークラスなので、当然自身は文字列である）、trueを返すhoge?メソッドが定義されている
+  def hoge?
+    self == "hoge"
+  end
 end
